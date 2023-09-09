931f343f55689f0b756661a31af73f87c3fc43b4
Hiyoco-Mame <1day.1code.c@gmail.com>
2023-09-09 20:01:40 +0900

    例15-8 追加

---
+++ Example_15_8/Main.java
@@ -0,0 +1,9 @@
+package Example_15_8;
+
+public class Main {
+    public static void main(String[] args){
+        String s = "abc,def;ghi";
+        String w = s.replaceAll("[beh]","X");
+        System.out.println(w);
+    }
+}
