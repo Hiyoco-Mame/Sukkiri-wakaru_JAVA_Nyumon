f8627fa8b0fa268bfa65aaf3097d50ffac1010cd
Hiyoco-Mame <1day.1code.c@gmail.com>
2023-09-09 20:00:50 +0900

    例15-4 追加

---
+++ Example_15_4/Main.java
@@ -0,0 +1,12 @@
+package Example_15_4;
+
+public class Main {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++){
+            sb.append("Java");
+        }
+        // String s = sb.toString();
+        System.out.println(sb);
+    }
+}
