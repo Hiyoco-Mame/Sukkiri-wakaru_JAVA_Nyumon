5411134e92a3b1b0e70c1c453ec1d43e91d1ba7f
Hiyoco-Mame <1day.1code.c@gmail.com>
2023-09-10 13:21:56 +0900

    15章　例題　15-12

---
+++ Example_15_12/Main.java
@@ -0,0 +1,20 @@
+package Example_15_12;
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args){
+        Calendar c = Calendar.getInstance();
+        c.set(2023,9,10,12,10,32);
+        c.set(Calendar.MONTH, 10);
+        
+        Date d = c.getTime();
+        System.out.println(c);
+
+        Date now = new Date();
+        c.setTime(now);
+        int y = c.get(Calendar.YEAR);
+        System.out.println("This year is " + y + ".");
+    }
+}
