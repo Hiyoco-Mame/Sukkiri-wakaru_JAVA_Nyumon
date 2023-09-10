b4b321c719e96a4857a2c62f2265dbb77e6363b3
Hiyoco-Mame <1day.1code.c@gmail.com>
2023-09-10 12:46:01 +0900

    15章　例題15-11

---
+++ Example_15_10/Main.java
@@ -0,0 +1,10 @@
+package Example_15_10;
+
+public class Main {
+    public static void main(String[] args){
+        long start = System.currentTimeMillis();
+
+        long end = System.currentTimeMillis();
+        System.out.println("処理にかかった時間んは・・・"+ (end - start) + "msでした");
+    }
+}
